1b277a71a6c5cc10e4c9c9bd0f396cfbeeb5984f
Rocio Sanchez <rocisa08@ucm.es>
2024-10-15 22:53:06 +0200

    new  Representations for Scottie1 signals

--- graph.py
+++ graph.py
@@ -17,22 +17,30 @@ timestamps = [i for i in range(samples) ]
 
 #plt.axis([0,100,0,4500])
 #plt.plot(timestamps,signal,'bo')
-plt.figure(figsize=(30, 15))
-plt.axis([0,4200,-3400,3400])
-plt.rcParams['path.simplify'] = True
-plt.rcParams['agg.path.chunksize'] = 850
+#plt.figure(figsize=(30, 15))
+#plt.axis([0,4200,-3400,3400])
+#plt.rcParams['path.simplify'] = True
+#plt.rcParams['agg.path.chunksize'] = 850
 signal = signal/100
 #poemos pintar de distintos colores las distintas partes de las señal
 #header = signal[]
 #plt.step(timestamps,signal)
 #podemos dibujar la frecuencia y el tiempo 
 #con el tiempo en segundos
-plt.axis([0,100,-3400,3400])
+#plt.axis([0,100,-3400,3400])
 #plt.plot(np.arange (len(signal))/samplerate,fft(signal))
 fig,(st,sf) = plt.subplots(2,1,constrained_layout=1)
 signalf = fft(signal)
 sf.plot(np.abs(signalf[:samplerate//2]),lw=0.15)
 sf.grid(1)
+
+samples  = signal.size
+seconds = samples/samplerate
+timestamps=[]
+timestamps = [i for i in range(samples) ]
+st.axis([0,4200,-3400,3400])
+
+st.step(timestamps,signal,lw = 0.25)
 
     # shows the plot  
     # in new window 
